3eaaa025aef76ac2a6323bde4889e0644cc372bc
xiechanglei <709955748@qq.com>
2023-09-19 13:33:12 +0800

    and yarn to create project

--- bin/create-xie-base.js
+++ bin/create-xie-base.js
@@ -1,15 +1,21 @@
-import {exec} from 'child_process';
-let currentPath = process.cwd();
-let command = 'mvn clean install';
-//执行命令,实时打印输出
-exec(command, {cwd: currentPath}, function (error, stdout, stderr) {
-    if (error) {
-        console.error('error: ' + error);
-        return;
+#!/usr/bin/env node
+let spawn = require('child_process').spawn;
+let path = require('path');
+let rootPath = path.resolve(__dirname, '../');
+//cd xie-base
+process.chdir(rootPath);
+
+const mvn = spawn('mvn', ['clean', 'install']);
+let message = '';
+const log = (buffer) => {
+    let msg = buffer.toString();
+    message += msg;
+    if (msg.endsWith('\n')) {
+        console.log(message.substring(0, message.length - 1));
+        message = '';
     }
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + typeof stderr);
-    if (typeof stderr == 'string') {
-        console.log('stderr: ' + stderr);
-    }
-});
+}
+mvn.stdout.on('data', log);
+mvn.stderr.on('data', log);
+mvn.on('close', () => console.log(`安装完成`));
+console.log('正在安装，请稍后...')
