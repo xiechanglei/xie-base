81f4620808097e1c49b23016327ccf861c83db6c
xiechanglei <709955748@qq.com>
2023-09-25 10:27:29 +0800

    增加文档注释

--- xie-base-common-http/src/main/java/com/xiechanglei/code/base/common/http/HttpHelper.java
+++ xie-base-common-http/src/main/java/com/xiechanglei/code/base/common/http/HttpHelper.java
@@ -2,20 +2,35 @@ package com.xiechanglei.code.base.common.http;
 
 import java.io.IOException;
 
-public class HttpTool {
+/**
+ * Http请求工具类，提供了一些常用的请求构建方法
+ */
+public class HttpHelper {
+    /**
+     * 构建一个请求，无任何默认配置
+     */
     public static HttpRequest build() {
         return new HttpRequest();
     }
 
+    /**
+     * 构建一个请求，模拟浏览器的请求头
+     */
     public static HttpRequest buildBrowserClient() {
         return new HttpRequest().transfer(trans -> trans.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
                 "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"));
     }
 
+    /**
+     * 根据url发起get请求，返回响应
+     */
     public static HttpResponse get(String url) throws IOException {
         return build().get(url);
     }
 
+    /**
+     * 模拟浏览器发起一个get请求，返回响应
+     */
     public static HttpResponse browserGet(String url) throws IOException {
         return buildBrowserClient().get(url);
     }
