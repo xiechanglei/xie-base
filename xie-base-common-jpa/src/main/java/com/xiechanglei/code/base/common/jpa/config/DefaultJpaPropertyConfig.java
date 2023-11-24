ee456652006c25c5ffaf489dd8f0694111397fd9
xiechanglei <709955748@qq.com>
2023-11-24 16:28:32 +0800

    jpa的自动发现应该写在用的地方

--- xie-base-common-jpa/src/main/java/com/xiechanglei/code/base/common/jpa/config/DefaultJpaPropertyConfig.java
+++ xie-base-common-jpa/src/main/java/com/xiechanglei/code/base/common/jpa/config/DefaultJpaPropertyConfig.java
@@ -1,15 +1,9 @@
 package com.xiechanglei.code.base.common.jpa.config;
 
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan
-@EnableJpaRepositories
-@EnableJpaAuditing
 @Configuration
 @PropertySource("classpath:xie.base.jpa.properties")
 public class DefaultJpaPropertyConfig {
